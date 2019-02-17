aa531ee0397990ac1283faf9830118141629bcde
Marvin Zachary <acerzach@gmail.com>
2019-02-17 22:37:36 +0300

    I declared a variab;e and a function to find the sum of diffrent inputed sides

---
+++ js/triangle.js
@@ -0,0 +1,11 @@
+var sideSum1 = function(firstvalue, secondvalue) {
+    return firstvalue + secondvalue;
+   };
+
+   var sideSum2 = function(secondvalue, thirdvalue) {
+    return secondvalue + thirdvalue;
+   };
+
+   var sideSum3 = function(firstvalue, thirdvalue) {
+    return firstvalue + thirdvalue;
+   };
