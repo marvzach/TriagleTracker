9cff9a8c3d3274d53b534972d9a07ce349987763
marvzach <37458467+marvzach@users.noreply.github.com>
2019-02-18 22:47:08 +0300

    Made changes to my javascript file including indention.

--- js/triangle.js
+++ js/triangle.js
@@ -1,39 +1,33 @@
-var sideSum1 = function(firstvalue, secondvalue) {
-    return firstvalue + secondvalue;
-   };
 
-   var sideSum2 = function(secondvalue, thirdvalue) {
-    return secondvalue + thirdvalue;
-   };
+       var tracker = function(firstvalue, secondvalue, thirdvalue) {
 
-   var sideSum3 = function(firstvalue, thirdvalue) {
-    return firstvalue + thirdvalue;
-   };
-   function triger(){
-    var array=['Its an Equilateral Triangle','Its an Isoscele Triangle','Its a Scalene Triangle']
-    var firstvalue=document.getElementById('one').value;
-    var secondvalue=document.getElementById('two').value;
-    var thirdvalue=document.getElementById('three').value;
-    var text;
-    if(sideSum1(firstvalue,secondvalue) <= thirdvalue || sideSum2(secondvalue,thirdvalue) <= firstvalue || sideSum3(firstvalue,thirdvalue) <= secondvalue){
-        text="It is not a triangle!"
-        document.getElementById("img").src= "images/not.png"
-    }
-    else if(firstvalue === secondvalue && secondvalue === thirdvalue){
-        text = array[0];
+         var firstvalue = parseInt(document.getElementById("one").value);
+         var secondvalue = parseInt(document.getElementById("two").value);
+         var thirdvalue = parseInt(document.getElementById("three").value);
+
+         var sides = [];
+         sides.push(firstvalue);
+         sides.push(secondvalue);
+         sides.push(thirdvalue);
+
+         if ((firstvalue + secondvalue> thirdvalue && firstvalue + thirdvalue > secondvalue && secondvalue + thirdvalue > firstvalue)
+         && (firstvalue=== secondvalue && secondvalue === thirdvalue)) {
+         document.getElementById("results").innerHTML = "Its an Equilateral Triangle of sides <br> " + sides + "";
          document.getElementById("img").src= "images/equ.jpg"
-    }
-     //Equivalent Triangle
-     else if(firstvalue===secondvalue || firstvalue===thirdvalue || secondvalue===thirdvalue) {
-         text=array[1];
-         document.getElementById("img").src= "images/isoseles.jpg"
-     }
-     //Isoscele Triangle
-     else if(firstvalue!==secondvalue && firstvalue!==thirdvalue &&secondvalue!==thirdvalue){
-             text=array[2];
-             document.getElementById("img").src= "images/sca.png"
-         }
-         //Scalene Triangle
+         } else if ((firstvalue + secondvalue > thirdvalue && firstvalue + thirdvalue > secondvalue && secondvalue + thirdvalue > firstvalue)
+          && ((firstvalue === secondvalue && secondvalue !== thirdvalue) ||
+         (firstvalue === thirdvalue && thirdvalue !== secondvalue) || (secondvalue=== thirdvalue && secondvalue !== firstvalue))) {
+           document.getElementById("results").innerHTML = "Its an Isoscele Triangle of sides <br> " + sides + "";
+            document.getElementById("img").src= "images/isoseles.jpg"
+         } else if ((firstvalue + secondvalue > thirdvalue && firstvalue + thirdvalue > secondvalue && secondvalue + thirdvalue > firstvalue)
+          && (firstvalue !== secondvalue && secondvalue !== thirdvalue && firstvalue !== thirdvalue)) {
+           document.getElementById("results").innerHTML = "Its a Scalene Triangle of sides <br> " + sides + "";
+           document.getElementById("img").src= "images/sca.png"
+         } else if (!firstvalue || !secondvalue || !thirdvalue) {
+           document.getElementById("results").innerHTML = "Enter all the sides and try again";
+         } else {
+           document.getElementById("results").innerHTML = " It is not a triangle!";
+           document.getElementById("img").src= "images/not.png"
 
-         document.getElementById('results').innerHTML = text;
+         }
        }
